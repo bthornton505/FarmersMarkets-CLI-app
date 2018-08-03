553cda07492427c19e2bd137155e77e113015a4f
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-03 15:13:37 +0000

    modified market_menu method, added some interface design

--- farmers_markets/lib/farmers_markets/cli.rb
+++ farmers_markets/lib/farmers_markets/cli.rb
@@ -8,17 +8,36 @@ class FarmersMarkets::CLI
   
   def list_markets # get markets 
     puts "Top 11 Denver Farmers Markets:"
-    puts "================================="
+    puts "======================================"
     puts <<-DOC
       1. Cherry Creek Fresh Market
       2. South Pearl Street Farmers Market
       3. City Park Esplanade Fresh Market
     DOC
-    puts "================================="
+    puts "======================================"
   end 
   
   def market_menu 
-    puts "Enter the number for the market you would like to learn more about:"
+    
+    input = nil 
+    while input != "exit"
+    puts "To learn more about a market, enter the number. To see the list again type list. If you wish to exit, type exit."
+    input = gets.strip 
+    puts "======================================"
+      case input 
+      when "1"
+        puts "More info on Cherry Creek Fresh Market"
+        puts "======================================"
+      when "2"
+        puts "More info on South Pearl Street Farmers Market"
+        puts "======================================"
+      when "3"
+        puts "More info on City Park Esplanade Fresh Market"
+        puts "======================================"
+      when "list"
+        list_markets
+      end 
+    end
   end 
   
 end 
