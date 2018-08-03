0bf2692c0c4e3a8a89c53062fc92f6e628a3ce7f
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-03 14:46:25 +0000

    added some details to user interface

--- farmers_markets/lib/farmers_markets/cli.rb
+++ farmers_markets/lib/farmers_markets/cli.rb
@@ -2,17 +2,19 @@
 class FarmersMarkets::CLI 
   
   def call 
-    puts "Top 11 Denver Farmers Markets:"
     list_markets
     market_menu 
   end 
   
   def list_markets # get markets 
+    puts "Top 11 Denver Farmers Markets:"
+    puts "================================="
     puts <<-DOC
       1. Cherry Creek Fresh Market
       2. South Pearl Street Farmers Market
       3. City Park Esplanade Fresh Market
     DOC
+    puts "================================="
   end 
   
   def market_menu 
