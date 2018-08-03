884798e808249086cba29eee76807a9912920406
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-03 23:45:49 +0000

    I created the first version of my market instances to get an idea of what I should be looking for when I scrape the data.

--- farmers_markets/lib/farmers_markets/market.rb
+++ farmers_markets/lib/farmers_markets/market.rb
@@ -1,13 +1,25 @@
 class FarmersMarkets::Market 
+  attr_accessor :name 
   
   def self.all_markets 
-    puts "======================================"
-    puts <<-DOC
-      1. Cherry Creek Fresh Market
-      2. South Pearl Street Farmers Market
-      3. City Park Esplanade Fresh Market
-    DOC
-    puts "======================================"
+    # puts "======================================"
+    # puts <<-DOC
+    #   1. Cherry Creek Fresh Market
+    #   2. South Pearl Street Farmers Market
+    #   3. City Park Esplanade Fresh Market
+    # DOC
+    # puts "======================================"
+    
+    market_1 = self.new 
+    market_1.name = "Cherry Creek Fresh Market"
+    
+    market_2 = self.new 
+    market_2.name = "South Pearl Street Farmers Market"
+    
+    market_3 = self.new 
+    market_3.name = "City Park Esplanade Fresh Market"
+    
+    [market_1, market_2, market_3]
   end 
   
 end 
