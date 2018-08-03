d494f75bb8edfef8ca9c8da52a3b991b47f7b192
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-03 14:41:44 +0000

    added a list_markets and market_menu methods

--- farmers_markets/lib/farmers_markets/cli.rb
+++ farmers_markets/lib/farmers_markets/cli.rb
@@ -4,10 +4,19 @@ class FarmersMarkets::CLI
   def call 
     puts "Top 11 Denver Farmers Markets:"
     list_markets
+    market_menu 
   end 
   
   def list_markets # get markets 
-    
+    puts <<-DOC
+      1. Cherry Creek Fresh Market
+      2. South Pearl Street Farmers Market
+      3. City Park Esplanade Fresh Market
+    DOC
+  end 
+  
+  def market_menu 
+    puts "Enter the number for the market you would like to learn more about:"
   end 
   
 end 
