ba632fb61ac204058d14700728fc14b6ef79a0f6
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-05 19:42:27 +0000

    This class was giving me some serious issues so I'm putting it on hold for now.

--- farmers_markets/lib/farmers_markets/market_scraper.rb
+++ farmers_markets/lib/farmers_markets/market_scraper.rb
@@ -1,22 +1,22 @@
-class MarketScraper
+# class MarketScraper
   
-  def self.scrape_markets
-    markets = []
+#   def self.scrape_markets
+#     markets = []
     
-    # market_1 = self.new 
-    # market_1.name = "Cherry Creek Fresh Market"
+#     # market_1 = self.new 
+#     # market_1.name = "Cherry Creek Fresh Market"
     
-    # market_2 = self.new 
-    # market_2.name = "South Pearl Street Farmers Market"
+#     # market_2 = self.new 
+#     # market_2.name = "South Pearl Street Farmers Market"
     
-    # market_3 = self.new 
-    # market_3.name = "City Park Esplanade Fresh Market"
+#     # market_3 = self.new 
+#     # market_3.name = "City Park Esplanade Fresh Market"
     
-    markets << self.scrape_tripsavvy 
-  end 
+#     markets << self.scrape_tripsavvy 
+#   end 
   
-  def self.scrape_tripsavvy
+#   def self.scrape_tripsavvy
     
-  end 
+#   end 
   
-end 
+# end 
