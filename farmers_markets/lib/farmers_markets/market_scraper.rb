f8af23ae9a049ec2231dea057ed7be006d389cbd
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-05 13:45:48 +0000

    I moved the code from market.rb into this new object which will scrape my website.

--- farmers_markets/lib/farmers_markets/market_scraper.rb
+++ farmers_markets/lib/farmers_markets/market_scraper.rb
@@ -1,16 +1,22 @@
 class MarketScraper
   
   def self.scrape_markets
-    market_1 = self.new 
-    market_1.name = "Cherry Creek Fresh Market"
+    markets = []
     
-    market_2 = self.new 
-    market_2.name = "South Pearl Street Farmers Market"
+    # market_1 = self.new 
+    # market_1.name = "Cherry Creek Fresh Market"
     
-    market_3 = self.new 
-    market_3.name = "City Park Esplanade Fresh Market"
+    # market_2 = self.new 
+    # market_2.name = "South Pearl Street Farmers Market"
     
-    [market_1, market_2, market_3]
+    # market_3 = self.new 
+    # market_3.name = "City Park Esplanade Fresh Market"
+    
+    markets << self.scrape_tripsavvy 
+  end 
+  
+  def self.scrape_tripsavvy
+    
   end 
   
 end 
