5547108b9e5ef20102c3a196e6c67492bda84c6e
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-05 20:59:56 +0000

    I worked on my scraper method and still need to find a way to iterate through the data to puts out each market in the terminal.

--- farmers_markets/lib/farmers_markets/market.rb
+++ farmers_markets/lib/farmers_markets/market.rb
@@ -1,5 +1,5 @@
 class FarmersMarkets::Market 
-  attr_accessor :name, :time, :description, :market, :url 
+  attr_accessor :name, :time, :description, :url, :markets  
   
   def self.all_markets 
     self.scrape_markets
@@ -13,14 +13,16 @@ class FarmersMarkets::Market
   
   def self.scrape_tripsavvy
     doc = Nokogiri::HTML(open("https://www.tripsavvy.com/farmers-markets-in-denver-1062032"))
-     
     
-    name = doc.css("h3#text-block_9-0").first.text
-    time = doc.css("div#inline-chop_2-0 p")[1].text
-    description = doc.css("div#inline-chop_2-0 p").first.text
-    url = doc.css("div#inline-chop_2-0 p a").first.attr('href')
-    binding.pry
-    
+    # doc.css("div.ordered-list__content").each do |market|
+      market = self.new 
+      market.name = doc.css("h3#text-block_9-0").first.text
+      market.time = doc.css("div#inline-chop_2-0 p")[1].text
+      market.description = doc.css("div#inline-chop_2-0 p").first.text
+      market.url = doc.css("div#inline-chop_2-0 p a").first.attr('href')
+      # binding.pry
+      market
+    # end
   end 
   
 end 
