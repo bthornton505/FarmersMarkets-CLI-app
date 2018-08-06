02db80dac9281c65555139f3bbcacbfdc931c7ba
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-06 14:37:06 +0000

    Tried to find a way to iterate through my website data but still not working. Using a binding.pry to help fix the problem.

--- farmers_markets/lib/farmers_markets/market.rb
+++ farmers_markets/lib/farmers_markets/market.rb
@@ -14,14 +14,16 @@ class FarmersMarkets::Market
   def self.scrape_tripsavvy
     doc = Nokogiri::HTML(open("https://www.tripsavvy.com/farmers-markets-in-denver-1062032"))
     
-    # doc.css("div.ordered-list__content").each do |market|
-      market = self.new 
-      market.name = doc.css("h3#text-block_9-0").first.text.strip 
-      market.time = doc.css("div#inline-chop_2-0 p")[1].text.strip 
-      market.description = doc.css("div#inline-chop_2-0 p").first.text.strip 
-      market.url = doc.css("div#inline-chop_2-0 p a").first.attr('href').strip 
+    # doc.css("div.ordered-list__content").each do |markets|
+    #   markets.css("div.loc affiliate-block").each do |market|
       # binding.pry
-      market
+        market = self.new 
+        market.name = doc.css("h3#text-block_9-0").first.text.strip 
+        market.time = doc.css("div#inline-chop_2-0 p")[1].text.strip 
+        market.description = doc.css("div#inline-chop_2-0 p").first.text.strip 
+        market.url = doc.css("div#inline-chop_2-0 p a").first.attr('href').strip 
+        market
+    #   end 
     # end
   end 
   
