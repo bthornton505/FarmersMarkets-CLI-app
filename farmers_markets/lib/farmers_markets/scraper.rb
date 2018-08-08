7e64b56a4a29887e04e6121d4554117139e84b7a
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-08 15:28:25 +0000

    started refactoring this class. removed #all_markets

--- farmers_markets/lib/farmers_markets/scraper.rb
+++ farmers_markets/lib/farmers_markets/scraper.rb
@@ -1,25 +1,15 @@
 class FarmersMarkets::Scraper 
   
-  def all_markets 
-    self.scrape_markets
-  end 
-  
-  def scrape_markets
-    markets = []
-    markets << self.scrape_tripsavvy 
-    markets 
-  end 
-  
   def scrape_tripsavvy
-    doc = Nokogiri::HTML(open("https://www.tripsavvy.com/farmers-markets-in-denver-1062032"))
+    @doc = Nokogiri::HTML(open("https://www.tripsavvy.com/farmers-markets-in-denver-1062032"))
 
     # doc.css("div#ordered-list__content").each do |market|
-      market_1 = self.new 
-      market_1.name = doc.css("h3#text-block_9-0").first.text.strip 
-      market_1.time = doc.css("div#inline-chop_2-0 p")[1].text.strip 
-      market_1.description = doc.css("div#inline-chop_2-0 p").first.text.strip 
-      market_1.url = doc.css("div#inline-chop_2-0 p a").first.attr('href').strip 
-      market_1
+      market = FarmersMarkets::Story.new 
+      market.name = doc.css("h3#text-block_9-0").first.text.strip 
+      market.time = doc.css("div#inline-chop_2-0 p")[1].text.strip 
+      market.description = doc.css("div#inline-chop_2-0 p").first.text.strip 
+      market.url = doc.css("div#inline-chop_2-0 p a").first.attr('href').strip 
+      market
     # end
   end 
   
