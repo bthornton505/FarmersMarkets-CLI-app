f429761609965134a94ac29ee4b85f8049da5675
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-08 18:47:49 +0000

    wrote an initializer for the market class.

--- farmers_markets/lib/farmers_markets/market.rb
+++ farmers_markets/lib/farmers_markets/market.rb
@@ -2,6 +2,13 @@ class FarmersMarkets::Market
   attr_accessor :name, :time, :description, :url  
   @@all = []
   
+  def intialize
+    @name = name 
+    @time = time 
+    @description = description
+    @url = url 
+  end 
+  
   def self.all
     @@all
   end 
