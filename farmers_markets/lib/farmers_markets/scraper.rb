568399f6f1fabfd76fc75a596f86df3ac51558f6
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-08 19:11:35 +0000

    refactored my code to finally be able to iterate through my website data and print out the markets in the proper order. SO HAPPY

--- farmers_markets/lib/farmers_markets/scraper.rb
+++ farmers_markets/lib/farmers_markets/scraper.rb
@@ -4,17 +4,13 @@ class FarmersMarkets::Scraper
     @doc = Nokogiri::HTML(open("https://www.tripsavvy.com/farmers-markets-in-denver-1062032"))
     
     @doc.search("div.ordered-list__content").each do |markets|
-      # binding.pry
       market = FarmersMarkets::Market.new 
       market.name = markets.search("h3").text.strip  
       market.time = markets.search("div#inline-chop_2-0 p")[1].text 
-      market.description = markets.search("div#inline-chop_2-0 p").text 
+      market.description = markets.search("div#inline-chop_2-0 p").first.text.strip 
       # market.url = markets.search("div#inline-chop_2-0 p a").attr('href').text
       
       market.save
     end
-  end 
-  
-  #doc.css("h3 span.link__wrapper").text (This gets the highlands ranch market. It has a link wrapped around it.)
-
+  end
 end 
