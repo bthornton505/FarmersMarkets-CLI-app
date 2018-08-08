2ccaf71b8bbaaf730322cd33743584ec9b5fea04
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-08 18:48:41 +0000

    introduced an iteration to #scrape_tripsavvy to go through all the markets in my website.

--- farmers_markets/lib/farmers_markets/scraper.rb
+++ farmers_markets/lib/farmers_markets/scraper.rb
@@ -2,15 +2,17 @@ class FarmersMarkets::Scraper
   
   def scrape_tripsavvy
     @doc = Nokogiri::HTML(open("https://www.tripsavvy.com/farmers-markets-in-denver-1062032"))
-
-    # doc.css("div#ordered-list__content").each do |market|
-      market = FarmersMarkets::Story.new 
-      market.name = doc.css("h3#text-block_9-0").first.text.strip 
-      market.time = doc.css("div#inline-chop_2-0 p")[1].text.strip 
-      market.description = doc.css("div#inline-chop_2-0 p").first.text.strip 
-      market.url = doc.css("div#inline-chop_2-0 p a").first.attr('href').strip 
-      market
-    # end
+    
+    @doc.search("div.ordered-list__content").each do |markets|
+      # binding.pry
+      market = FarmersMarkets::Market.new 
+      market.name = markets.search("h3").text.strip  
+      market.time = markets.search("div#inline-chop_2-0 p")[1].text 
+      market.description = markets.search("div#inline-chop_2-0 p").text 
+      # market.url = markets.search("div#inline-chop_2-0 p a").attr('href').text
+      
+      market.save
+    end
   end 
   
   #doc.css("h3 span.link__wrapper").text (This gets the highlands ranch market. It has a link wrapped around it.)
