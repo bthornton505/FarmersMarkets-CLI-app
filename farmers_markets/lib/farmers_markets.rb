0815f899e35c4e8627b92496c5c5af99c4203a61
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-09 13:45:08 +0000

    cleaned up this code to look better.

--- farmers_markets/lib/farmers_markets.rb
+++ farmers_markets/lib/farmers_markets.rb
@@ -3,11 +3,7 @@ require 'open-uri'
 require 'pry'
 require 'colorize'
 
-
-
-module FarmersMarketsCLI 
-  require_relative "./farmers_markets/version"
-  require_relative './farmers_markets/cli'
-  require_relative './farmers_markets/market'
-  require_relative './farmers_markets/scraper'
-end
+require_relative "./farmers_markets/version"
+require_relative './farmers_markets/cli'
+require_relative './farmers_markets/market'
+require_relative './farmers_markets/scraper'
